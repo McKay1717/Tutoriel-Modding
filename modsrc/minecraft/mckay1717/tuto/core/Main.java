b4206b01e3aa32103113fcbb8345f31c5d8c1a8b
Nicolas <nicolas1706ni@gmail.com>
2013-08-26 14:10:14 +0200

    Episode 4

--- modsrc/minecraft/mckay1717/tuto/core/Main.java
+++ modsrc/minecraft/mckay1717/tuto/core/Main.java
@@ -3,6 +3,7 @@ package mckay1717.tuto.core;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
 import mckay1717.tuto.proxy.CommonProxy;
 import mckay1717.tuto.block.BlockJump;
 import mckay1717.tuto.core.*;
@@ -15,7 +16,7 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 
-@Mod(modid="tuto", name="Tutoriel Modding 1.6", version="Episode 3")
+@Mod(modid="tuto", name="Tutoriel Modding 1.6", version="Episode 4")
 @NetworkMod(clientSideRequired=true, serverSideRequired=false)
 public class Main {
 
@@ -29,6 +30,7 @@ public class Main {
 	private static int id = 1266;
 	
 	public static Block block = (new BlockJump(id+1,Material.rock).setUnlocalizedName("tuto:l16").func_111022_d("tuto:l16").setLightValue(1f).setResistance(5f).setHardness(0).setStepSound(Block.soundAnvilFootstep).setCreativeTab(CreativeTabs.tabBlock));
+	public static Item item = (new Item(4000).setUnlocalizedName("tuto:item").func_111206_d("tuto:item"));
 
 
 
@@ -43,6 +45,7 @@ public class Main {
 		proxy.proxy();
 		System.out.println("Init");
 		mckay1717.tuto.core.RegisterTuto.registerBlock(block, "Lanterne en Stone");
+		mckay1717.tuto.core.RegisterTuto.registerItem(item, "Item Test");
 	}
 
 	@EventHandler
